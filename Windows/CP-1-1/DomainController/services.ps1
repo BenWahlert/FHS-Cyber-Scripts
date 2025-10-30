e6c4a25acb1726fbcdb01cf9a280d74af07476cb
moonmantee <jwan56w@gmail.com>
2025-10-30 10:56:22 -0600

    Added Domain Controller files copied from Member Server files

---
+++ Windows/CP-1-1/DomainController/services.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Unblock-File -Path .\services.ps1
+Set-ExecutionPolicy -ExecutionPolicy bypass
+get-service | Format-Table -Autosize
+
+Get-Content -Path ~\Downloads\cp\Windows10\badservices.txt | Stop-Service
+Get-Content -Path ~\Downloads\cp\Windows10\goodservices.txt | Start-Service
