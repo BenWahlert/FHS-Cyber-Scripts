ba9c52b825739645a85095055aa724dcc9a7e15c
acamq <=>
2025-10-30 20:05:36 -0600

    Point legacy Ubuntu scripts at local repo assets

--- Linux/Old-Ubuntu-Script/main1.sh
+++ Linux/Old-Ubuntu-Script/main1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ###
 # # (From Bash)
-# source <(curl -fsSL https://github.com/wen012235/CP/blob/main/main.sh)
+# source <(curl -fsSL https://raw.githubusercontent.com/BenWahlert/FHS-Cyber-Scripts/main/Linux/Old-Ubuntu-Script/main1.sh)
 ###
 
 if [ "$EUID" -ne 0 ] ;
@@ -12,8 +12,14 @@ fi
 
 sudo apt update
 sudo apt install -y git
-mkdir -p ~/tmp/cp
-cd ~/tmp/cp
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+default_root="$HOME/FHS-Cyber-Scripts/Linux/Old-Ubuntu-Script"
+legacy_root="$script_dir"
+
+if [ -d "$default_root" ]; then
+    legacy_root="$default_root"
+fi
 UFWDEFAULT='/etc/default/ufw'
 if ! test -f "$UFWDEFAULT"; then
     echo "$UFWDEFAULT does not exist."
@@ -22,7 +28,7 @@ if ! test -f "$UFWDEFAULT"; then
       echo 'ufw package is not installed.'
     fi
 
-    return
+    exit 0
   fi
 
   sed -i 's/IPT_SYSCTL=.*/IPT_SYSCTL=\/etc\/sysctl\.conf/' "$UFWDEFAULT"
@@ -30,25 +36,24 @@ if ! test -f "$UFWDEFAULT"; then
 
 
 
-git clone https://github.com/wen012235/CP.git
 echo "Would you like some CP Goodness?********removing mp3 and other files, removing bad programs, removing guest users, running rkhunter, and chrootkit******"
         select yn in "Yes" "No"; do
             case $yn in
-                Yes )#bash ~/tmp/cp/CP/debloat.sh
-		bash ~/tmp/cp/CP/CPgoodies1.sh
+                Yes )#bash "$legacy_root/debloat.sh"
+		bash "$legacy_root/CPgoodies1.sh"
 	chmod 777 /etc/security/pwquality.conf
 	rm -f /etc/security/pwquality.conf
-	cp -f ~/tmp/cp/CP/pwquality.conf /etc/security/pwquality.conf 
+	cp -f "$legacy_root/pwquality.conf" /etc/security/pwquality.conf 
 	chmod 777 /etc/pam.d/common-password
 	rm -f /etc/pam.d/common-password
-	cp -f ~/tmp/cp/CP/common-password /etc/pam.d/common-password
+	cp -f "$legacy_root/common-password" /etc/pam.d/common-password
 	#echo 'minclass=4' >> /etc/security/pwquality.conf
 	chmod 644 /etc/security/pwquality.conf
 	chmod 644 /etc/pam.d/common-password
                 firefox_root="/usr/lib/firefox"
                 if [ -d "$firefox_root" ]; then
-                    install -m 644 ~/tmp/cp/CP/mozilla.cfg "$firefox_root/mozilla.cfg"
-                    install -D -m 644 ~/tmp/cp/CP/autoconfig.js "$firefox_root/defaults/pref/autoconfig.js"
+                    install -m 644 "$legacy_root/mozilla.cfg" "$firefox_root/mozilla.cfg"
+                    install -D -m 644 "$legacy_root/autoconfig.js" "$firefox_root/defaults/pref/autoconfig.js"
                 else
                     echo "Firefox not found at $firefox_root; skipping Firefox configuration copy."
                 fi
@@ -66,8 +71,8 @@ if [[ $(lsb_release -rs) == "16.04" ]]; then
 	echo "Press 1 for a standard harden and 2 for a full harden"
         select yn in "1" "2"; do
             case $yn in
-                1 ) bash ~/tmp/cp/CP/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl1_workstation; break;;
-                2 ) bash ~/tmp/cp/CP/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl2_workstation; break;;
+                1 ) bash "$legacy_root/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh" lvl1_workstation; break;;
+                2 ) bash "$legacy_root/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh" lvl2_workstation; break;;
         
 	
 		
@@ -80,8 +85,8 @@ elif [[ $(lsb_release -rs) == "18.04" ]]; then
 	echo "Press 1 for a standard harden and 2 for a full harden"
 	select yn in "1" "2"; do
 	    case $yn in
-		1 ) bash ~/tmp/cp/CP/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl1_workstation; break;;
-		2 ) bash ~/tmp/cp/CP/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl2_workstation; break;;
+		1 ) bash "$legacy_root/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh" lvl1_workstation; break;;
+		2 ) bash "$legacy_root/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh" lvl2_workstation; break;;
 	    esac
 	done
 echo "done with Ubuntu 18 hardening"
@@ -89,7 +94,7 @@ echo "done with Ubuntu 18 hardening"
 
 elif [[ $(lsb_release -rs) == "20.04" ]]; then
 	echo "Press 3 for a standard harden and 4 for a full harden"
-	bash ~/tmp/cp/CP/ubuntu20/UBUNTU2004_LBK.sh
+	bash "$legacy_root/ubuntu20/UBUNTU2004_LBK.sh"
 echo "done with Ubuntu 20 hardening"
 fi
 
