b61df3911061bea4a228c265fa9be5b041f95684
moonmantee <jwan56w@gmail.com>
2025-10-30 19:08:37 -0600

    edited main.sh to clone correct directory

--- Linux/Old-Ubuntu-Script/main.sh
+++ Linux/Old-Ubuntu-Script/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ###
 # # (From Bash)
-# source <(curl -fsSL https://github.com/wen012235/CP/blob/main/main.sh)
+# source <(curl -fsSL https://github.com/BenWahlert/FHS-Cyber-Scripts/main/main.sh)
 ###
 
 if [ "$EUID" -ne 0 ] ;
@@ -17,19 +17,16 @@ cd ~/tmp/cp || exit
 
 
 
-git clone https://github.com/wen012235/CP.git
+git clone https://github.com/BenWahlert/FHS-Cyber-Scripts.git
 
 #!/usr/bin/env bash
 if [[ $(lsb_release -rs) == "16.04" ]]; then
 	echo "Press 1 for a standard harden and 2 for a full harden"
         select yn in "1" "2"; do
             case $yn in
-                1 ) bash ~/tmp/cp/CP/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl1_workstation; break;;
-                2 ) bash ~/tmp/cp/CP/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl2_workstation; break;;
-        
-	
-		
-	    esac
+                1 ) bash ~/tmp/cp/Linux/Old-Ubuntu-Script/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl1_workstation; break;;
+                2 ) bash ~/tmp/cp/Linux/Old-Ubuntu-Script/ubuntu16/cis-hardening/Canonical_Ubuntu_16.04_CIS_v1.1.0-harden.sh lvl2_workstation; break;;
+            esac
         done
 echo "done with Ubuntu 16 hardening"
 
@@ -38,8 +35,8 @@ elif [[ $(lsb_release -rs) == "18.04" ]]; then
 	echo "Press 1 for a standard harden and 2 for a full harden"
 	select yn in "1" "2"; do
 	    case $yn in
-		1 ) bash ~/tmp/cp/CP/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl1_workstation; break;;
-		2 ) bash ~/tmp/cp/CP/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl2_workstation; break;;
+		1 ) bash ~/tmp/cp/Linux/Old-Ubuntu-Script/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl1_workstation; break;;
+		2 ) bash ~/tmp/cp/Linux/Old-Ubuntu-Script/ubuntu18/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS_v1.0.0-harden.sh lvl2_workstation; break;;
 	    esac
 	done
 echo "done with Ubuntu 18 hardening"
@@ -47,14 +44,14 @@ fi
 echo "Would you like some CP Goodness?********removing mp3 and other files, removing bad programs, removing guest users, running rkhunter, and chrootkit******"
         select yn in "Yes" "No"; do
             case $yn in
-                Yes )bash ~/tmp/cp/CP/CPgoodies1.sh
+                Yes )bash ~/tmp/cp/Linux/Old-Ubuntu-Script/CPgoodies1.sh
 	#chmod 777 /etc/security/pwquality.conf
-	#cp -f ~/tmp/cp/CP/pwquality.conf /etc/security/pwquality.conf 
+	#cp -f ~/tmp/cp/Linux/Old-Ubuntu-Script/pwquality.conf /etc/security/pwquality.conf 
 	#echo 'minclass=4' >> /etc/security/pwquality.conf
 	#chmod 644 /etc/security/pwquality.conf
-	cp -f ~/tmp/cp/CP/mozilla.cfg /usr/lib/firefox/mozilla.cfg 
+	cp -f ~/tmp/cp/Linux/Old-Ubuntu-Script/mozilla.cfg /usr/lib/firefox/mozilla.cfg 
 	chmod 644 /usr/lib/firefox/mozilla.cfg
-	cp -f ~/tmp/cp/CP/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js 
+	cp -f ~/tmp/cp/Linux/Old-Ubuntu-Script/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js 
 	chmod 644 /usr/lib/firefox/defaults/pref/autoconfig.js; break;;
 	        No ) echo "ok"; break;;
             esac
