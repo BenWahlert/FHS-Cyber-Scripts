8c0c5f6752e608b15597523e5d1b76df27fbea9a
acamq <al.charmquark@gmail.com>
2025-11-01 18:49:47 -0600

    Fix file paths, update services.ps1 to stop and disable services from Windows\CP-1-1\Desktop\badservices.txt

--- Windows/CP-1-1/Server/services.ps1
+++ Windows/CP-1-1/Server/services.ps1
@@ -3,5 +3,12 @@ Unblock-File -Path .\services.ps1
 Set-ExecutionPolicy -ExecutionPolicy bypass
 get-service | Format-Table -Autosize
 
-Get-Content -Path ~\Downloads\cp\Windows10\badservices.txt | Stop-Service
-Get-Content -Path ~\Downloads\cp\Windows10\goodservices.txt | Start-Service
+$badServicesPath = '~\Downloads\cp\Windows\CP-1-1\Desktop\badservices.txt'
+$badServices = Get-Content -Path $badServicesPath | Where-Object { -not [string]::IsNullOrWhiteSpace($_) } | ForEach-Object { $_.Trim() }
+
+$badServices | Stop-Service
+Get-Content -Path ~\Downloads\cp\Windows\CP-1-1\Desktop\goodservices.txt | Start-Service
+Foreach ($Service in $badServices) {
+		Set-Service -Name $Service -StartupType 'Disabled'
+		Stop-Service -Name $Service -Force
+	}
